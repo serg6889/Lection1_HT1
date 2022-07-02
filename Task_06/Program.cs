b0392f5e579bb14c404924da4484885049ecf14e
Sergey68 <sesergey@yandex.ru>
2022-07-02 11:42:45 +1100

    File is updated with Task_06

---
+++ Task_06/Program.cs
@@ -0,0 +1,14 @@
+﻿// Task_06
+
+int numA = new Random().Next(-10, 10);
+Console.Write("numA = ");
+Console.WriteLine(numA);
+
+if(numA % 2 == 0)
+{
+    Console.WriteLine("numA is even number");
+}
+else
+{
+    Console.WriteLine("numA is odd number");
+}
