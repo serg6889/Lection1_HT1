8c0f0519e0551c8219839712af142ac36f5d098c
Sergey68 <sesergey@yandex.ru>
2022-07-02 11:03:37 +1100

    File is updated with Task_04_4_5_6

---
+++ Task_04/Program.cs
@@ -0,0 +1,26 @@
+﻿// Task_04_5_6
+
+//int a = 2;
+//int b = 3;
+//int c = 7;
+
+//int a = 44;
+//int b = 5;
+//int c = 78;
+
+int a = 22;
+int b = 3;
+int c = 9;
+
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+
+Console.WriteLine(max);
+
+
+
